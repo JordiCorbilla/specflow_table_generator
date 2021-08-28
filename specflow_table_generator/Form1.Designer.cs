15a13ed56e6dc3a949039d32138c38fb350a1f27
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-08-28 19:12:05 +0100

    Adding first draft of the tool

---
+++ specflow_table_generator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace specflow_table_generator
+{
+    partial class SpecFlowTableGenerator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpecFlowTableGenerator));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SqlQuery = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ConnectionString = new System.Windows.Forms.Label();
+            this.results = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.SqlQuery);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.ConnectionString);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1303, 225);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1040, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(928, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SqlQuery
+            // 
+            this.SqlQuery.AutoSize = true;
+            this.SqlQuery.Location = new System.Drawing.Point(12, 46);
+            this.SqlQuery.Name = "SqlQuery";
+            this.SqlQuery.Size = new System.Drawing.Size(63, 15);
+            this.SqlQuery.TabIndex = 4;
+            this.SqlQuery.Text = "SQL Query";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 43);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(804, 179);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(804, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Data Source=localhost;Initial Catalog=DatingApp;Integrated Security=True";
+            // 
+            // ConnectionString
+            // 
+            this.ConnectionString.AutoSize = true;
+            this.ConnectionString.Location = new System.Drawing.Point(12, 17);
+            this.ConnectionString.Name = "ConnectionString";
+            this.ConnectionString.Size = new System.Drawing.Size(100, 15);
+            this.ConnectionString.TabIndex = 1;
+            this.ConnectionString.Text = "ConnectionString";
+            // 
+            // results
+            // 
+            this.results.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.results.Location = new System.Drawing.Point(0, 225);
+            this.results.Multiline = true;
+            this.results.Name = "results";
+            this.results.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.results.Size = new System.Drawing.Size(1303, 570);
+            this.results.TabIndex = 4;
+            // 
+            // SpecFlowTableGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1303, 795);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.panel1);
+            this.Name = "SpecFlowTableGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SpeckFlow Table Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label SqlQuery;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label ConnectionString;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox results;
+    }
+}
+
---
+++ specflow_table_generator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using Dapper;
+
+namespace specflow_table_generator
+{
+    public partial class SpecFlowTableGenerator : Form
+    {
+        public SpecFlowTableGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            results.Clear();
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                results.AppendText("Please fill in the connection string property!");
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                results.AppendText("Please fill in the SQL Query string property!");
+                return;
+            }
+
+            GetSqlData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            results.Clear();
+        }
+
+        private void GetSqlData()
+        {
+            IEnumerable<IDictionary<string, object>> table;
+            using (var connection = new SqlConnection(textBox1.Text))
+            {
+                connection.Open();
+                table = connection.Query(textBox2.Text) as IEnumerable<IDictionary<string, object>>;
+            }
+
+            foreach (var row in table)
+            {
+                //then iterate the columns and get a KeyValuePair for each column
+                foreach (var col in row)
+                {
+                    results.AppendText($"{col.Key} {col.Value}{Environment.NewLine}");
+                }
+            }
+        }
+    }
+}
